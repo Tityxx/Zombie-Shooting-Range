f8725f8070783bf8085437ada104709f283e3231
Tityxx <nikitka_nik1@mail.ru>
2022-03-07 22:35:34 +0700

    WIP Спавн боссов

---
+++ Assets/GameResources/Scripts/Spawners/BossSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ToolsAndMechanics.ObjectPool;
+using UnityEngine;
+
+/// <summary>
+/// Спавн босса
+/// </summary>
+public class BossSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private PoolableObjectData data;
+    [SerializeField]
+    private AnimationCurve countCurve;
+    [SerializeField]
+    private AnimationCurve spawnDelayCurve;
+    [SerializeField]
+    private Transform[] spawnPoints;
+
+    private ObjectPoolController pool;
+    private float startTime;
+
+    private void Start()
+    {
+        pool = FindObjectOfType<ObjectPoolController>();
+
+        startTime = Time.time;
+    }
+
+    private IEnumerator SpawnCoroutine()
+    {
+        while (enabled)
+        {
+            yield return new WaitForSeconds(1);
+
+
+        }
+    }
+
+    private void Spawn(Vector3 position)
+    {
+        GameObject boss = pool.GetObject(data, position);
+        boss.GetComponent<EnemyController>().SetPath();
+    }
+}
