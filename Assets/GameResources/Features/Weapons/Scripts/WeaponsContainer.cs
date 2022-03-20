64cc68671fd31201aba4b795c0bdde0a9a34cd04
Tityxx <nikitka_nik1@mail.ru>
2022-03-20 16:21:01 +0700

    UI

---
+++ Assets/GameResources/Features/Weapons/Scripts/WeaponsContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Контейнер оружий
+/// </summary>
+[CreateAssetMenu(fileName = "Weapons Container", menuName = "Game/Weapons Container Data")]
+public class WeaponsContainer : ScriptableObject
+{
+    public IReadOnlyList<WeaponData> Weapons => weapons;
+
+    [SerializeField]
+    private List<WeaponData> weapons;
+}
