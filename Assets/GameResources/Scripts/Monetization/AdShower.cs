07a458020e521a879593a1bc9244c45bbdfbabe7
Nikita.Sukhodub <syhodyb99@gmail.com>
2022-07-17 02:29:30 +0700

    Fix interstitial. Remove Reward button

--- Assets/GameResources/Scripts/Monetization/AdShower.cs
+++ Assets/GameResources/Scripts/Monetization/AdShower.cs
@@ -1,21 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class AdShower
+public class AdShower : MonoBehaviour
 {
-    private static bool isSub;
+    public static AdShower Instance { get; private set; }
+
+    private YandexSDK sdk => YandexSDK.instance;
+
+    private static float lastInterstitialTime;
+
+    private const float MINIMUM_INTERSTITIAL_DELAY = 6f;
     private const string REWARD_KEY = "Reward";
+
+    private void Start()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        lastInterstitialTime = Time.time;
+
+        sdk.onRewardedAdReward += OnCloseReward;
+        
+        sdk.onUserDataReceived += OnSDKNull;
+        sdk.onInterstitialShown += OnSDKNull;
+        sdk.onInterstitialFailed += OnSDKNull;
+        sdk.onRewardedAdOpened += OnSDKNull;
+        sdk.onRewardedAdClosed += OnSDKNull;
+        sdk.onRewardedAdError += OnSDKNull;
+        sdk.onPurchaseSuccess += OnSDKNull;
+        sdk.onPurchaseFailed += OnSDKNull;
+        sdk.onClose += OnSDKNull;
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+            sdk.onRewardedAdReward -= OnCloseReward;
+            
+            sdk.onUserDataReceived -= OnSDKNull;
+            sdk.onInterstitialShown -= OnSDKNull;
+            sdk.onInterstitialFailed -= OnSDKNull;
+            sdk.onRewardedAdOpened -= OnSDKNull;
+            sdk.onRewardedAdClosed -= OnSDKNull;
+            sdk.onRewardedAdError -= OnSDKNull;
+            sdk.onPurchaseSuccess -= OnSDKNull;
+            sdk.onPurchaseFailed -= OnSDKNull;
+            sdk.onClose -= OnSDKNull;
+        }
+    }
 
     public static void ShowAd()
     {
+        if (lastInterstitialTime + MINIMUM_INTERSTITIAL_DELAY > Time.time) return;
+
+        lastInterstitialTime = Time.time;
+
         if (Application.isEditor)
         {
             Debug.Log("SHOW AD!");
         }
         else
         {
-            YandexSDK.instance?.ShowInterstitial();
+            YandexSDK.instance.ShowInterstitial();
         }
     }
     public static void ShowRewardAd()
@@ -23,22 +81,36 @@ public static class AdShower
         if (Application.isEditor)
         {
             Debug.Log("SHOW REWARD!");
-            OnCloseReward(REWARD_KEY);
+            Instance.OnCloseReward(REWARD_KEY);
         }
         else
         {
-            if (!isSub)
-            {
-                YandexSDK.instance.onRewardedAdReward += OnCloseReward;
-                isSub = true;
-            }
-            YandexSDK.instance?.ShowRewarded(REWARD_KEY);
+            YandexSDK.instance.ShowRewarded(REWARD_KEY);
         }
     }
 
-    private static void OnCloseReward(string key)
+    private void OnCloseReward(string key)
     {
-        if (key == REWARD_KEY)
-            Currency.Value += 500;
+        Currency.Value += 500;
+    }
+
+    private void OnOpenReward(int obj)
+    {
+    }
+
+    private void OnCloseRewardFail(int obj)
+    {
+    }
+
+    private void OnSDKNull(string obj)
+    {
+    }
+
+    private void OnSDKNull(int i)
+    {
+    }
+
+    private void OnSDKNull()
+    {
     }
 }
